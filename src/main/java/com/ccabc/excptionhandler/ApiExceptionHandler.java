17bba76e9a50846b7d6a908219763f9e0632f6b6
Zartab Nakhwa <zartab@codewithz.com>
2025-05-16 10:49:55 +0530

    Updtae- Added Exception Handler

--- src/main/java/com/ccabc/excptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/ccabc/excptionhandler/ApiExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.ccabc.excptionhandler;
 
 import com.ccabc.payload.ApiExceptionPayload;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,20 +12,20 @@ import org.springframework.web.server.ResponseStatusException;
 public class ApiExceptionHandler {
 
     @ExceptionHandler(value = {Exception.class})
-    public ResponseEntity<ApiExceptionPayload> handleAPIException(ResponseStatusException exception, WebRequest webRequest){
-        String path=webRequest.getDescription(true);
+    public ResponseEntity<ApiExceptionPayload> handleAPIException(Exception exception, WebRequest webRequest) {
+        String path = webRequest.getDescription(false);
 
-//        Creating the payload
-        ApiExceptionPayload apiExceptionPayload=new ApiExceptionPayload();
-        apiExceptionPayload.setMessage(exception.getReason());
-        apiExceptionPayload.setStatusCode(exception.getStatusCode().value());
-        apiExceptionPayload.setHttpStatus(exception.getStatusCode().toString());
+        // Creating the payload
+        ApiExceptionPayload apiExceptionPayload = new ApiExceptionPayload();
+        apiExceptionPayload.setMessage(exception.getMessage());
+        apiExceptionPayload.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        apiExceptionPayload.setHttpStatus(HttpStatus.INTERNAL_SERVER_ERROR.toString());
         apiExceptionPayload.setSuccess(false);
         apiExceptionPayload.setException(true);
         apiExceptionPayload.setTimestamp(java.time.LocalDateTime.now());
         apiExceptionPayload.setPath(path);
 
-//        Returning the payload
-        return new ResponseEntity<>(apiExceptionPayload, exception.getStatusCode());
+        // Returning the payload
+        return new ResponseEntity<>(apiExceptionPayload, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
